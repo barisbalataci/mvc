1fdecbb538d163129dd53f8d520b2770d95f484a
barisbalataci <barisbalataci@gmail.com>
2017-02-12 02:29:45 +0300

    .

--- Nortwind.MvcWebUI/DependencyResolver/NinjectControllerFactory.cs
+++ Nortwind.MvcWebUI/DependencyResolver/NinjectControllerFactory.cs
@@ -14,18 +14,19 @@ namespace Nortwind.MvcWebUI.DependencyResolver
     public class NinjectControllerFactory:DefaultControllerFactory
 
     {
-        private IKernel _kernel;
+        private readonly IKernel _kernel;
         public NinjectControllerFactory()
         {
-            _kernel = new StandardKernel(new BusinessModule());
+            _kernel = new StandardKernel();
             //_kernel.Bind<IProductService>().To<ProductManager>().InSingletonScope();
             //_kernel.Bind<IProductDal>().To<EfProductDal>().InSingletonScope();
             //_kernel.Bind<ICategoryService>().To<CategoryManager>().InSingletonScope();
             //_kernel.Bind<ICategoryDal>().To<EfCategoryDal>().InSingletonScope();
             //_kernel.Bind<IUserService>().To<UserManager>().InSingletonScope();
             //_kernel.Bind<IUserDal>().To<EfUserDal>().InSingletonScope();
-            //_kernel.Bind<IProjectService>().To<ProjectService>().InSingletonScope();
+            _kernel.Bind<IProjectService>().To<ProjectService>().InSingletonScope();
         }
+       
         protected override IController GetControllerInstance(RequestContext requestContext, Type controllerType)
         {
             return controllerType == null ? null : (IController)_kernel.Get(controllerType);
